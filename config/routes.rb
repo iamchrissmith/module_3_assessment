353df700808d8d62fd27ac19ea31328e34d026e8
Chris Smith <42@chrisryansmith.com>
2017-07-26 10:53:58 -0600

    add search spec and search bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'items#index'
 
+  get "search" => "search#index", :as => :search
+
+
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :items, only: [:create, :show, :index, :destroy]
---
+++ spec/features/user_can_search_within_zip_for_store_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature 'User can search for a store in ZIP' do
+  scenario 'user sees first 10 stores within 25 miles' do
+    visit root_path
+
+    fill_in "Find Stores near your ZIP", with: "80202"
+    click_button "Search"
+
+    expect(current_path).to eq search_path
+
+    stores = page.all('.store')
+    expect(stores.count).to eq 10
+    expect(page).to have_content 'Stores within 25 miles of 80202'
+    expect(page).to have_content '17 Total Stores'
+    expect(stores.first).to have_content "Name:"
+    expect(stores.first).to have_content "City:"
+    expect(stores.first).to have_content "Distance:"
+    expect(stores.first).to have_content "Phone Number:"
+    expect(stores.first).to have_content "Store Type:"
+    # As a user
+    # When I visit "/"
+    # And I fill in a search box with "80202" and click "search"
+    # Then my current path should be "/search" (ignoring params)
+    # And I should see stores within 25 miles of 80202
+    # And I should see a message that says "17 Total Stores"
+    # And I should see exactly 10 results (There are 17 stores within 25 miles. We want to display 10 on this page and 7 on the next. Get the first page to work first from start to finish and worry about pagination later.)
+    # And I should see the long name, city, distance, phone number and store type for each of the 10 results
+  end
+end
